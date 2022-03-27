3830dc9691a6f51a6972d6da6192694c5d0cd9f2
inferno964 <aaradhayforearn11@gmail.com>
2022-03-27 22:30:22 +0530

    lavender: Aex-ify
    Signed-off-by: inferno964 <aaradhayforearn11@gmail.com>

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Pixel Experience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common AEX stuff
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -22,10 +22,14 @@ PRODUCT_NAME := aosp_lavender
 PRODUCT_MODEL := Redmi Note 7
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
 TARGET_VENDOR_PRODUCT_NAME := lavender
+EXTENDED_BUILD_TYPE := OFFICIAL
 
+# Build with GApps if GAPPS_BUILD is true
+ifeq ($(GAPPS_BUILD),true)
+    WITH_GAPPS := true
+    TARGET_GAPPS_ARCH := arm64
+    IS_PHONE := true
+    TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
+endif
